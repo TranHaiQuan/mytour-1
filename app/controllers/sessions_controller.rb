d7d99e7096fd7dfbd37d7e05b915abd3e827f401
tran hai quan <tranhaiquan.thq@gmail.com>
2017-12-26 23:11:28 +0700

    Log in

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = t "controllers.sessions_controller.logged_in"
+      log_in user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+      remember user
+      redirect_to root_path
+    else
+      flash.now[:danger] = t "controllers.sessions_controller.invalid"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
+end
